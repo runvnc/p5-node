1bf68738c3db40fe1cdc83cc1bb89b036b3a5e31
Jason Livesay <runvnc@gmail.com>
2022-12-07 05:58:43 -0600

    update

--- zstdtest2.js
+++ zstdtest2.js
@@ -6,7 +6,7 @@ const {spawn} = require('child_process')
 async function getZSTDdata(filename) {
   if (fs.existsSync(filename)) {
     // Set the high water mark for the input stream.
-    const inputStream = fs.createReadStream(filename, { highWaterMark: 16000000 });
+   // const inputStream = fs.createReadStream(filename, { highWaterMark: 16000000 });
 
     const bufferPromise = new Promise((resolve, reject) => {
       // Set the high water mark for the output stream.
@@ -15,9 +15,9 @@ async function getZSTDdata(filename) {
       }, { highWaterMark: 16000000 });
 
       // Set the high water mark for the decompress stream.
-      const zstd = spawn('zstd', ['-d'])
-      zstd.stdin.highWaterMark = 16000000
-      inputStream.pipe(zstd.stdin,{highWaterMark:16000000})
+      const zstd = spawn('zstd', ['-d', filename])
+      //zstd.stdin.highWaterMark = 16000000
+      //inputStream.pipe(zstd.stdin,{highWaterMark:16000000})
       zstd.stdout.highWaterMark = 16000000
       zstd.stdout.pipe(outputStream)
       zstd.stderr.on('data', (d) => {
